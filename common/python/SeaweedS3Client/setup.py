8311183043f7a4265c4a0ce982d7388105ff23f2
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-12-04 20:39:58 +0300

    Fix: Fixed the url of the package in the setup tools

--- common/python/SeaweedS3Client/setup.py
+++ common/python/SeaweedS3Client/setup.py
@@ -4,7 +4,7 @@ setup(
     name="SeaweedS3Client",
     version="0.0.1",
     long_description="A python package for interacting with SeaweedFS S3 API. It wraps the Boto3 module and is geared towards our AutoML Platform.",
-    url="git@github.com:WildCayote/SeaweedS3Client.git",
+    url="git@github.com:WildCayote/AutoML.git",
     author="Tinsae Shemalise",
     author_email="tinsaeshemalise@gmail.com",
     license="unlicense",
