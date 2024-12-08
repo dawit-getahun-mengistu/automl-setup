d2180431d4e831bc3d29f0f7140157daf78b5d88
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-12-08 20:23:27 +0300

    Feat: Added a code snippet for creating an s3 client

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from "@nestjs/common";
 import { SeaweedOptions } from "../interfaces/seaweed-module-options";
+import { S3Client } from "@aws-sdk/client-s3";
 
 @Injectable()
 export class StorageService {
+  private client: S3Client;
+
   constructor(private options: SeaweedOptions) {
     this.createConnection();
+    console.log(this.client);
   }
 
   private createConnection() {
-    console.log(this.options);
+    this.client = new S3Client({
+      credentials: {
+        accessKeyId: this.options.access_key,
+        secretAccessKey: this.options.secret_key,
+      },
+      endpoint: this.options.s3_endpoint,
+    });
   }
 }
