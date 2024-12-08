3bcb782235da2f1a0a822c8601ef1d2fd94b245f
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-12-08 20:15:25 +0300

    Fix: Removed old services

--- src/interfaces/random-number-service-options.ts
+++
@@ -1,9 +0,0 @@
-export interface RandomNumberServiceOptions {
-  min: number;
-  max: number;
-}
-
-export const defaultRandomNumberServiceOptions: RandomNumberServiceOptions = {
-  min: 0,
-  max: 1000,
-};
